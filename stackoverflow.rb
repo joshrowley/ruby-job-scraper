4c641d661ca0ee7e21f86846b65066025e54ea5c
Josh Rowley <joshua.f.rowley@gmail.com>
2012-10-16 14:59:20 -0400

    Write initial pseudocode for stackoverflow scraper

---
+++ stackoverflow.rb
@@ -0,0 +1,16 @@
+# TODO: Require necessary libraries for nokogiri sqlite3 and anything else
+
+# Nice to have: dynamic location and distance range to create query to Stackoverflow board (i.e. this file receives 'new york' as the location and '20' as the mile range which to look at on stackoverflow)
+
+
+
+# TODO: Load main Stack Overflow into Nokogiri
+
+
+# TODO: Parse out the links from the main index page to the individual jobs pages, put this into an array
+#Check if there's another page
+#If there is go to the link on that page and add those job links into the individual jobs array as well.
+
+
+# TODO: Iterate through the array, pull out the necessary variables for the database.
+#TODO: Save to the database
