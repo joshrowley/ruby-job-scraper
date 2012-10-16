4d9541cd7d9c27154312df0e2c08d98aed455e9e
Josh Rowley <joshua.f.rowley@gmail.com>
2012-10-16 16:59:26 -0400

    Separate db creation into own file db_create.rb

--- stackoverflow.rb
+++ stackoverflow.rb
@@ -1,32 +1,13 @@
 require 'open-uri'
 require 'nokogiri'
 require 'sqlite3'
-require 'FileUtils'
+
+
+
+
+require_relative 'db_create.rb'
 
 # Nice to have: dynamic location and distance range to create query to Stackoverflow board (i.e. this file receives 'new york' as the location and '20' as the mile range which to look at on stackoverflow)
-
-FileUtils.rm("jobs.db") if File.exists?("jobs.db")
-
-db = SQLite3::Database.new "jobs.db"
-
-sql = <<SQL
-CREATE table jobs 
-(	id INTEGER PRIMARY KEY,
-	title TEXT,
-	employer TEXT,
-	employer_link TEXT,
-	location TEXT,
-	description TEXT,
-	skills TEXT,
-	about_employer TEXT
-	);
-SQL
-
-db.execute sql
-
-# Nice to have: Put this database creation logic in another spot
-
-
 
 # TODO: Load main Stack Overflow into Nokogiri
 uri = "http://careers.stackoverflow.com/jobs?searchTerm=ruby&location=new+york&range=20"
@@ -43,7 +24,6 @@ end
 #If there is go to the link on that page and add those job links into the individual jobs array as well.
 
 
-# TODO: Iterate through the array, pull out the necessary variables for the database.
 
 job_links.each do |link|
 	doc = Nokogiri::HTML(open(link))
@@ -65,13 +45,7 @@ job_links.each do |link|
 					end
 	
 
-
-
-
-
-#TODO: Save to the database
-
-db.execute("INSERT INTO jobs (title, employer, employer_link, location, description, skills, about_employer) VALUES (?, ?, ?, ?, ?, ?, ?)", title, employer, employer_link, location, description, skills, about_employer)
+@db.execute("INSERT INTO jobs (title, link, employer, employer_link, location, description, skills, about_employer) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", title, link, employer, employer_link, location, description, skills, about_employer)
 
 end
 
